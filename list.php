8518bbeaac281a720c7b347987fd37f8925f7764
Islam <islam@inaya.edu.sa>
2025-10-23 08:16:23 +0300

    fixing list file

--- list.php
+++ list.php
@@ -1,9 +1,11 @@
 <?php 
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
