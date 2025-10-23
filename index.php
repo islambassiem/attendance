7ee5551fab7d60effc06346e4b2be9b88677c7d0
Islam <islam@inaya.edu.sa>
2025-10-23 09:24:24 +0300

    centering the QR code

--- index.php
+++ index.php
@@ -6,6 +6,20 @@ require 'google.php';
 <html>
 <head>
   <title>Workshop Attendance</title>
+      <style>
+    body {
+      font-family: Arial, sans-serif;
+      text-align: center;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+    }
+  </style>
 </head>
 <body>
   <img 
